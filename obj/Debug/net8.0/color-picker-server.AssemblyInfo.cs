ac58aab33e2c2acaeafc0edf92a8e712b446494b
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-06-15 15:36:30 +0100

    Start creating models and DbContext

--- obj/Debug/net8.0/color-picker-server.AssemblyInfo.cs
+++ obj/Debug/net8.0/color-picker-server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("color-picker-server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cb4c2e2d966d0f318c6d7bd634d338b764ae79d")]
 [assembly: System.Reflection.AssemblyProductAttribute("color-picker-server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("color-picker-server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
