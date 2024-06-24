73bd48ff3d0f10c2bf4be3c14db353b94c6ff23c
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-06-24 13:07:03 +0100

    Add properties to Study model

--- Models/Study.cs
+++ Models/Study.cs
@@ -6,5 +6,10 @@ namespace color_picker_server.Models
     public string Title { get; set; }
     public string OriginalLink { get; set; }
     public DateTime DateUploaded { get; set; }
+    public string ImageLink { get; set; }
+    public string ThumbnailLink { get; set; }
+    public required string UserId { get; set; }
+    public User? User { get; set; }
+    public ICollection<Note> Notes { get; } = new List<Note>();
   }
 }
