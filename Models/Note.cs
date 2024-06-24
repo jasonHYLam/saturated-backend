c8a3986aa296ef0545b158788b9286ac6b7d4061
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-06-24 13:11:36 +0100

    Add properties to Note model

--- Models/Note.cs
+++ Models/Note.cs
@@ -3,6 +3,14 @@ namespace color_picker_server.Models
   public class Note
   {
     public int Id { get; set; }
-    // public
+    public string Text { get; set; }
+
+    // Colors are stored in hex format
+    public string OriginalHexColor { get; set; }
+    public string GuessedHexColor { get; set; }
+
+    public int StudyId { get; set; }
+
+    // Add normalised coordinates
   }
 }
