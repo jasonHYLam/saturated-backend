85a6b7b7cec8c64eaf105d5db6a38b703c97472a
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-06-24 13:43:12 +0100

    Add Psql database to builder

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
+using color_picker_server.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
+builder.Services.AddDbContext<DBContext>(
+  options => options.UseNpgsql(connectionString));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
