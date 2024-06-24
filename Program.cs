4c2f8fa7ceeee20ffc730ba055c3ad972a7fee7a
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-06-24 14:13:14 +0100

    Add Cloudinary dependencies

--- Program.cs
+++ Program.cs
@@ -1,8 +1,28 @@
 using color_picker_server.Models;
 using Microsoft.EntityFrameworkCore;
+using dotenv.net;
+using CloudinaryDotNet;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Cloudinary set up
+// =================
+DotEnv.Load(options: new DotEnvOptions(probeForEnv: true));
+
+Account account = new Account(
+  Environment.GetEnvironmentVariable("CLOUDINARY_NAME"),
+  Environment.GetEnvironmentVariable("CLOUDINARY_API_KEY"),
+  Environment.GetEnvironmentVariable("CLOUDINARY_API_SECRET")
+);
+Cloudinary cloudinary = new Cloudinary(account);
+cloudinary.Api.Secure = true;
+builder.Services.AddSingleton(cloudinary);
+// =================
+
+
+
+// CORS set up
+// =================
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 builder.Services.AddCors(options =>
 {
@@ -16,17 +36,22 @@ builder.Services.AddCors(options =>
       .AllowCredentials();
     });
 });
+// =================
 
+// DB set up
+// =================
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
 builder.Services.AddDbContext<DBContext>(
   options => options.UseNpgsql(connectionString)
   );
+// =================
 
 builder.Services.AddAuthorization();
 builder.Services.AddIdentityApiEndpoints<User>()
 .AddEntityFrameworkStores<DBContext>();
 
 // Swagger support
+// =================
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument(config =>
 {
@@ -34,6 +59,7 @@ builder.Services.AddOpenApiDocument(config =>
   config.Title = "Lovely Rita";
   config.Version = "v1";
 });
+// =================
 
 var app = builder.Build();
 
--- obj/Debug/net8.0/color-picker-server.AssemblyInfo.cs
+++ obj/Debug/net8.0/color-picker-server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("color-picker-server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0780e7d90260c9291b81850b084a2bf5b0f1514")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7b99ec20500b4a651a2405cca8ce18c081e2c29")]
 [assembly: System.Reflection.AssemblyProductAttribute("color-picker-server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("color-picker-server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
