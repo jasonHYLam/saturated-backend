b0780e7d90260c9291b81850b084a2bf5b0f1514
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-06-24 13:52:48 +0100

    Add CORS and Identitysupport

--- Program.cs
+++ Program.cs
@@ -3,10 +3,32 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy(
+    name: MyAllowSpecificOrigins,
+    policy =>
+    {
+      policy.WithOrigins(["http://localhost:5173"])
+      .AllowAnyHeader()
+      .AllowAnyMethod()
+      .AllowCredentials();
+    });
+});
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
 builder.Services.AddDbContext<DBContext>(
-  options => options.UseNpgsql(connectionString));
+  options => options.UseNpgsql(connectionString)
+  );
+
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<User>()
+.AddEntityFrameworkStores<DBContext>();
+
+
 var app = builder.Build();
+app.MapIdentityApi<User>();
 
 app.MapGet("/", () => "Hello World!");
 
