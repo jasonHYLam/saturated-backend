bfdee0ef1befe849f259375cd74f3f8de188ef31
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-06-25 20:41:44 +0100

    Add constructor to NoteController

--- Controllers/NoteController.cs
+++ Controllers/NoteController.cs
@@ -1,6 +1,25 @@
+using CloudinaryDotNet;
+using color_picker_server.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace color_picker_server.Controllers;
 
 public class NoteController : ControllerBase
-{ }
+{
+
+  private readonly Cloudinary _cloudinary;
+  private readonly UserManager<User> _userManager;
+  private readonly DBContext _context;
+
+  public NoteController(
+    DBContext context,
+    UserManager<User> userManager,
+    Cloudinary cloudinary
+    )
+  {
+    _context = context;
+    _userManager = userManager;
+    _cloudinary = cloudinary;
+  }
+}
