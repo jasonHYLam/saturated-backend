343e3d7c54d2ddfe8f27e8781dd6e02b31365b4e
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-06-25 20:37:50 +0100

    Start creating NoteController

---
+++ Controllers/NoteController.cs
@@ -0,0 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace color_picker_server.Controllers;
+
+public class NoteController : ControllerBase
+{ }
--- obj/Debug/net8.0/color-picker-server.AssemblyInfo.cs
+++ obj/Debug/net8.0/color-picker-server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("color-picker-server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab0dcaba82119df6bafb9cb14e4d67e4d246146c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdb892f5624e4feba4e5a2a7d40e6b475937606f")]
 [assembly: System.Reflection.AssemblyProductAttribute("color-picker-server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("color-picker-server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
